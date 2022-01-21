fc0e3bbf7c78d3c4559304b746aaa656a420f691
timy116 <timy117@gmail.com>
2022-01-21 20:02:05 +0800

    Renamed app and updated url rules

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import include, url
+from django.urls import path
 
 urlpatterns = [
+    url(r"^", include("apps.portal.urls")),
     path('admin/', admin.site.urls),
 ]
 
