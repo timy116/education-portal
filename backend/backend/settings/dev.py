98688dbebb3eae73eef2b3133491286afd016091
timy116 <timy117@gmail.com>
2022-01-21 18:19:43 +0800

    Updated make file and add some debug tools

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -1,1 +1,28 @@
 from .base import *
+
+
+# Debug toolbar settings
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("application/javascript", ".js", True)
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_COLLAPSED": True,
+}
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+
+# kolo settings.
+MIDDLEWARE += [
+    "kolo.middleware.KoloMiddleware",
+]
