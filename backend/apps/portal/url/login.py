af979048d9d04c32ab1ce7fca5418bec991e9cbe
timy116 <timy117@gmail.com>
2022-01-26 17:56:32 +0800

    Finished teacher login page

--- backend/apps/portal/url/login.py
+++ backend/apps/portal/url/login.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from ..views.login import IndependentStudentLoginView
+
+from ..views.login import (
+    IndependentStudentLoginView, TeacherLoginView
+)
 
 urlpatterns = [
     url(r"^independent/$", IndependentStudentLoginView.as_view(), name="independent_student_login"),
+    url(r"^teacher/$", TeacherLoginView.as_view(), name="teacher_login"),
 ]
--- backend/apps/portal/views/login.py
+++ backend/apps/portal/views/login.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.views import LoginView
 
 from ..forms.student import IndependentStudentLoginForm
+from ..forms.teacher import TeacherLoginForm
 
 
 class IndependentStudentLoginView(LoginView):
     template_name = "login/independent_student.html"
     form_class = IndependentStudentLoginForm
+
+
+class TeacherLoginView(LoginView):
+    template_name = "login/teacher.html"
+    form_class = TeacherLoginForm
