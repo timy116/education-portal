3f8384ddb50b471dcce4f9c6d130d95af984f311
timy116 <timy117@gmail.com>
2022-01-30 16:31:59 +0800

    Add more factories

--- backend/tests/factories.py
+++ backend/tests/factories.py
@@ -1,7 +1,9 @@
 import threading
-
+import uuid
 import factory
 from django.conf import settings
+from django.utils import timezone
+import datetime
 
 
 class Factory(factory.django.DjangoModelFactory):
@@ -29,3 +31,31 @@ class UserFactory(Factory):
     username = factory.Sequence(lambda n: f"user{n}")
     email = factory.LazyAttribute(lambda obj: f"{obj.username}@email.com")
     password = factory.PostGeneration(lambda obj, *args, **kwargs: obj.set_password(obj.username))
+
+
+class EmailVerificationFactory(Factory):
+    class Meta:
+        strategy = factory.CREATE_STRATEGY
+        model = "portal.EmailVerification"
+
+    user = factory.SubFactory("tests.factories.UserFactory")
+    token = factory.LazyAttribute(lambda obj: uuid.uuid4().hex[:30])
+    email = factory.LazyAttribute(lambda obj: obj.user.email)
+    expiry = factory.LazyAttribute(lambda obj: timezone.now() + datetime.timedelta(hours=1))
+
+
+class UserProfileFactory(Factory):
+    class Meta:
+        strategy = factory.CREATE_STRATEGY
+        model = "portal.UserProfile"
+
+    user = factory.SubFactory("tests.factories.UserFactory")
+
+
+class TeacherFactory(Factory):
+    class Meta:
+        strategy = factory.CREATE_STRATEGY
+        model = "portal.Teacher"
+
+    user_profile = factory.SubFactory("tests.factories.UserProfileFactory")
+    user = factory.SubFactory("tests.factories.UserFactory")
