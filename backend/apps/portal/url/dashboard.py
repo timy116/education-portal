218370a8cd35991a73bb7d4606b62b8a6c37e07d
timy116 <timy117@gmail.com>
2022-03-02 01:33:22 +0800

    Add teacher dashboard create organisation view
    - Add `create_organisation` work flow(for the request method is 'POST')

--- backend/apps/portal/url/dashboard.py
+++ backend/apps/portal/url/dashboard.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from ..views.dashboard import (
-    IndependentStudentDashboard, organisation_create
+    IndependentStudentDashboard, create_organisation, create_class
 )
 
 urlpatterns = [
     url(r"^independent/$", IndependentStudentDashboard.as_view(), name="independent_student_dashboard"),
-    url(r"^onboarding-organisation/$", organisation_create, name="onboarding_organisation"),
+    url(r"^onboarding-organisation/$", create_organisation, name="onboarding_organisation"),
+    url(r"^onboarding-class/$", create_class, name="onboarding_class"),
 ]
--- backend/apps/portal/views/dashboard.py
+++ backend/apps/portal/views/dashboard.py
@@ -1,10 +1,15 @@
-from .mixins import LoginRequiredNotRaiseErrorMixin
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from ..permissions import independent_student_login
+from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect
+
+from .mixins import LoginRequiredNotRaiseErrorMixin
 from ..forms.teacher import OrganisationJoinForm, OrganisationForm
+from ..models import School
+from ..permissions import independent_student_login, teacher_login
 
 
 class IndependentStudentDashboard(LoginRequiredNotRaiseErrorMixin, UserPassesTestMixin, TemplateView):
@@ -15,10 +20,37 @@ class IndependentStudentDashboard(LoginRequiredNotRaiseErrorMixin, UserPassesTes
         return independent_student_login(self.request.user)
 
 
-def organisation_create(request):
+@login_required(login_url=reverse_lazy("teacher_login"))
+@user_passes_test(teacher_login, login_url=reverse_lazy("teacher_login"))
+def create_organisation(request):
     teacher = request.user.teacher
     create_form = OrganisationForm(user=request.user)
     join_form = OrganisationJoinForm()
+
+    if request.method == "POST":
+        if "create_organisation" in request.POST:
+            create_form = OrganisationForm(request.POST, user=request.user)
+
+            if create_form.is_valid():
+                data = create_form.cleaned_data
+                name = data.get("name", "")
+                postcode = data.get("postcode", "").upper()
+                country = data.get("country", "")
+                town, lat, lng = ("", "0", "0")
+                school = School.objects.create(
+                    name=name,
+                    postcode=postcode,
+                    town=town,
+                    latitude=lat,
+                    longitude=lng,
+                    country=country,
+                )
+                teacher.school = school
+                teacher.is_admin = True
+                teacher.save()
+                messages.success(request=request, message=f"您的學校 '{teacher.school.name}' 已成功建立。")
+
+                return HttpResponseRedirect(reverse_lazy("onboarding_class"))
 
     return render(
         request=request,
@@ -29,3 +61,9 @@ def organisation_create(request):
             "teacher": teacher,
         },
     )
+
+
+@login_required(login_url=reverse_lazy("teacher_login"))
+@user_passes_test(teacher_login, login_url=reverse_lazy("teacher_login"))
+def create_class(request):
+    pass
